c0cb0979b1a53707252b069ada883099715b0623
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-12-28 14:42:08 -0500

    Add bootstrap checks for Homebrew and Go in check.sh

--- check.sh
+++ check.sh
@@ -5,6 +5,26 @@ echo "-= Regular maintenance"
 DOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo
+echo "-=-= Bootstrap"
+
+# Check for brew
+if ! command -v brew &>/dev/null; then
+    echo "✗ - brew is missing, install it with:"
+    echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    exit 1
+fi
+
+# Check for go
+if ! command -v go &>/dev/null; then
+    echo "✓ - brew is installed but go is missing"
+    echo " temporarily install it with (it may be replaced with an asdf version later):"
+    echo " brew install go"
+    exit 1
+else
+    echo "✓ - brew is installed and go is available"
+fi
+
+echo
 ./installDotLinks.sh
 
 echo
